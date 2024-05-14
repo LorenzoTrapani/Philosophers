0fce3e3a9c8351abe1bd6fbdb8da861d1fb7255a
lotrapan <lotrapan@gmail.com>
2024-05-14 22:07:42 +0200

    da norminettare

--- src/main.c
+++ src/main.c
@@ -6,18 +6,11 @@
 /*   By: lotrapan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 17:53:48 by lotrapan          #+#    #+#             */
-/*   Updated: 2024/05/14 17:07:30 by lotrapan         ###   ########.fr       */
+/*   Updated: 2024/05/14 22:07:08 by lotrapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-//DATA RACE = two or more threads want to access a shared resource at the same time.
-/* ac 1 = num of PHILOSOPHERS(same as forks)
-ac 2 = The time a philosopher will die if he doesn’t eat
-ac 3 = The time it takes a philosopher to eat
-ac 4 = The time it takes a philosopher to sleep
-ac 5 = OPZIONALE Number of times all the philosophers need to eat before terminating the program */
 
 int main(int ac, char **av)
 {
